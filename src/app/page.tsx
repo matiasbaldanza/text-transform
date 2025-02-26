ccb629719cf2ccdf75a28b36dc8a5206d4b92b39
Matias Baldanza <matiasbaldanza@gmail.com>
2025-02-26 00:12:04 -0300

    fix: ts2322 as TemplateString is not directly assignable to ReactNode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import { metadata } from "./layout";
+
 export default function Home() {
+  const { title, description } = metadata;
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <h1 className="text-4xl font-bold">{metadata.title}</h1>
-      <p className="text-lg">{metadata.description}</p>
+      <h1 className="text-4xl font-bold">{title as string}</h1>
+      <p className="text-lg">{description}</p>
     </div>
   );
 }
